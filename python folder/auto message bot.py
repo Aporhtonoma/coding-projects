80aaf1fa7af5dabfe7c514974a5ac7e151af4eba
Aporhtonoma <101722867+Aporhtonoma@users.noreply.github.com>
2024-02-29 12:54:29 +0200

    Update auto message bot.py

--- python folder/auto message bot.py
+++ python folder/auto message bot.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
-msg = input("Enter the message: ")
-n = input("How many times ?: ")
-time.sleep(5)
+msg = input("Enter the message: ")   #message string
+n = input("How many times ?: ")     #how many times it will repeat
+time.sleep(5)     #5 second delay so it dosent immidiately start
 for i in range(0,int(n)):
-     pyautogui.typewrite(msg + '\n')
+     pyautogui.typewrite(msg + '\n') #message typign
